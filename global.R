894e75399c44822132c21099c19e7c0436326645
John Helsel <john.helsel@wsp.com>
2019-05-30 11:22:07 -0700

    code review

--- global.R
+++ global.R
@@ -1,6 +1,6 @@
 # install R packages required to run the app, if library does not exist
 list.of.packages <- c("shiny", "leaflet", "tidyverse", "stringr", "ggmap", 
-                      "rgdal", "rgeos", "shinydashboard", "data.table", "shinyFile")
+                      "rgdal", "rgeos", "shinydashboard", "data.table", "shinyFiles")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 
 if(length(new.packages)) install.packages(new.packages)
